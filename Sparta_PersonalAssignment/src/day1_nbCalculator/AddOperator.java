4d7f5158443d231441b642112448cb3cea825113
dami0806 <dami0804@naver.com>
2024-05-01 21:30:25 +0900

    Level2 끝코드 정리

--- Sparta_PersonalAssignment/src/day1_nbCalculator/AddOperator.java
+++ Sparta_PersonalAssignment/src/day1_nbCalculator/AddOperator.java
@@ -5,10 +5,5 @@ public class AddOperator implements Operator {
     public double operator(double firstNumber, double secondNumber) {
         return firstNumber + secondNumber;
     }
-
-    @Override
-    public String getSymbol() {
-        return "+";
-    }
 }
 
