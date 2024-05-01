5a71650518209d5c4310737be148d2609c81dba1
dami0806 <dami0804@naver.com>
2024-05-01 18:25:26 +0900

    Level 2 - 계산기 분리
    App기능 -> Calculator 기능

--- Sparta_PersonalAssignment/src/day1_nbCalculator/App.java
+++ Sparta_PersonalAssignment/src/day1_nbCalculator/App.java
@@ -28,11 +28,11 @@ public class App {
 
             result = calculator.calculate(firstNumber, secondNumber, symbol);
 
-            app.addArr(result);
+            calculator.addArr(result);
             System.out.println("결과: " + result);
 
-            app.removeArr();
-            app.inquiry();
+            calculator.removeArr();
+            calculator.inquiry();
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             if (getInput(br).toLowerCase().equals("exit")) {
@@ -41,30 +41,7 @@ public class App {
         }
     }
 
-    private void inquiry() {
-        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-
-        for (int i : results) {
-            System.out.printf("%d ", i);
-            System.out.println("");
-        }
-    }
-
-    private void addArr(int result) {
-        results.add(result);
-    }
-
-    private void removeArr() throws IOException {
-        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-        if (getInput(br).toLowerCase().equals("remove")) {
-            if (results.isEmpty()) {
-                System.out.println("결과배열이 비어있습니다.");
-            }
-            results.remove(0);
-        }
-    }
-
-    private static String getInput(BufferedReader br) throws IOException {
+    static String getInput(BufferedReader br) throws IOException {
         return br.readLine();
     }
 
@@ -89,29 +66,4 @@ public class App {
             }
         }
     }
-
-//    private int calculatorResult(int first, int second, String symbol) throws IOException {
-//        int result = 0;
-//        switch (symbol) {
-//            case "+":
-//                calculator. result = first + second;
-//                break;
-//            case "-":
-//                result = first - second;
-//                break;
-//            case "*":
-//                result = first * second;
-//                break;
-//            case "/":
-//                while (second == 0) {
-//                    System.out.println("0이 아닌 두번째 연산자입력를 다시 입력해주세요:");
-//                    second = inputNumber(getInput(br));
-//                }
-//
-//                result = first / second;
-//                break;
-//        }
-//        return result;
-//    }
 }
-
--- Sparta_PersonalAssignment/src/day1_nbCalculator/Calculator.java
+++ Sparta_PersonalAssignment/src/day1_nbCalculator/Calculator.java
@@ -1,6 +1,53 @@
 package day1_nbCalculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static day1_nbCalculator.App.br;
+import static day1_nbCalculator.App.getInput;
+
 public class Calculator {
+    /* 연산 결과를 저장하는 컬렉션 타입 필드를 외부에서 직접 접근 하지 못하도록 수정*/
+    ArrayList<Integer> results = new ArrayList<>();
+    private String symbol;
+    private int firstNumber, secondNumber, result;
+
+    // getter setter
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+
+    // 연산 메서드
     private int add(int first, int second) {
         return first + second;
     }
@@ -20,7 +67,31 @@ public class Calculator {
         return first / second;
     }
 
-    public int calculate(int first, int second, String operator) {
+    public void inquiry() {
+        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+
+        for (int i : results) {
+            System.out.printf("%d ", i);
+            System.out.println("");
+        }
+    }
+
+    public void addArr(int result) {
+        results.add(result);
+    }
+
+    public void removeArr() throws IOException {
+        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+        if (getInput(br).toLowerCase().equals("remove")) {
+            if (results.isEmpty()) {
+                System.out.println("결과배열이 비어있습니다.");
+            }
+            results.remove(0);
+        }
+    }
+
+    public int calculate(int first, int second, String operator) throws IOException {
+
         int result = 0;
         switch (operator) {
             case "+":
@@ -38,6 +109,7 @@ public class Calculator {
             case "/":
                 while (second == 0) {
                     System.out.println("0이 아닌 두번째 연산자입력를 다시 입력해주세요:");
+                    second = Integer.parseInt(getInput(br));
                 }
                 result = divide(first, second);
                 break;
