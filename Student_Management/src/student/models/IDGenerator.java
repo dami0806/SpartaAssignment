6f2e70f406377a912585bf5d2f0616e900871231
dami0806 <dami0804@naver.com>
2024-05-03 01:10:17 +0900

    fix: 고유번호 번호 재사용성 추가

--- Student_Management/src/student/models/IDGenerator.java
+++ Student_Management/src/student/models/IDGenerator.java
@@ -1,20 +1,54 @@
 package student.models;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 //싱글톤 관리 인스턴스, 현재id
+/*
+
+ */
 public class IDGenerator {
     private static IDGenerator instance;
     private int currentId;
+    private Set<Integer> allocatedIds; // 사용중인 id
+    private TreeSet<Integer> freeIds; // 사용됐다가 삭제 된 id
+
+    private IDGenerator() {
+        allocatedIds = new HashSet<>();
+        freeIds = new TreeSet<>();
+        currentId = 1;  // ID 할당을 1부터 시작
+    }
 
     // 인스턴스 갖기
-    public static IDGenerator getInstance() {
-        if(instance == null) {
+    public static synchronized IDGenerator getInstance() {
+        if (instance == null) {
             instance = new IDGenerator();
         }
         return instance;
     }
 
-    // 고유번호를 생성하고 반환하는 메서드
-    public synchronized int generateId(){ //getCurrentId() {
+    public synchronized int generateId() {
+
+        // 삭제된 id 확인
+        if (!freeIds.isEmpty()) {
+            int id = freeIds.pollFirst();
+            allocatedIds.contains(id);
+            return id;
+        }
+        //없으면 이제 새 id 사용
+        while (allocatedIds.contains(currentId)) {
+            currentId++;
+        }
+        allocatedIds.add(currentId);
         return currentId++;
+
+    }
+
+    //사용끝난후 해제
+    public synchronized void freeId(int id) {
+        if(allocatedIds.remove(id)) {
+            freeIds.add(id);
+        }
     }
 }
