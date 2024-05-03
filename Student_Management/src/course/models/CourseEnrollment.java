f0ee08ac0fcd2a1cf529fd6666525a029d3199a8
dami0806 <dami0804@naver.com>
2024-05-03 18:29:01 +0900

    feat: 과목별 점수등록

--- Student_Management/src/course/models/CourseEnrollment.java
+++ Student_Management/src/course/models/CourseEnrollment.java
@@ -3,6 +3,7 @@ package course.models;
 import Score.models.Score;
 
 import java.lang.reflect.AnnotatedType;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CourseEnrollment {
@@ -13,7 +14,46 @@ public class CourseEnrollment {
         return scoresBySession;
     }
 
-    public CourseEnrollment(Course course) {
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
         this.course = course;
+    }
+
+    public void setScoresBySession(Map<Integer, Score> scoresBySession) {
+        this.scoresBySession = scoresBySession;
+    }
+
+    public CourseEnrollment(Course course, Map<Integer, Score> scoresBySession) {
+        this.course = course;
+        this.scoresBySession = scoresBySession;
+    }
+
+
+    // 점수 추가
+    public void addScore(int session, int score) throws Exception {
+        // 점수 정의
+        if (session < 1 || session > 10) {
+            throw new Exception("유효한 섹션 범위를 지정해주세요(1-10)");
+        }
+        // 중복성
+        if (scoresBySession.containsKey(session)) {
+            throw new Exception("이전 섹션의 점수가 있습니다");
+        }
+        if (score < 0 || score > 100) {
+            throw new Exception("유효한 점수를 지정해주세요(0-100)");
+        }
+        scoresBySession.put(session, new Score(score, "A"));
+        //추가를 하기
+
+    }
+
+    // 점수 수정
+    public void updateScore(int session, int score) throws Exception {
+        if (scoresBySession == null) {
+            throw new Exception("score doesn't exist");
+        }
     }
 }
